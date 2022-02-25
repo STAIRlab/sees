6f74497a015ef0fb2050afc5754877fe4dad1f60
claudio perez <50180406+claudioperez@users.noreply.github.com>
2022-02-25 07:22:57 -0800

    cmp - add apidoc script

--- tools/doc.py
+++ tools/doc.py
@@ -1,27 +1,65 @@
+import re
+import sys
 import inspect
-from opensees import arg, obj
+import textwrap
+import importlib
+
+
+from opensees import ast, obj
+
+
+write = lambda *args, **kwds: print(*args, **kwds)
 
 if __name__=="__main__":
     import opensees.lib
 
-    for lib in [opensees.lib, opensees.patch]:
+    #for lib in [opensees.section, opensees.lib, opensees.patch]:
+    for lib_name in sys.argv[1:]:
+        lib = importlib.import_module(lib_name)
         for k,v in lib.__dict__.items():
             if isinstance(v, type):
                 if issubclass(v, obj.Component):
                     #print(k)
                     s = str(inspect.signature(v))
-                    print(f"{k}{s.replace('=None','')}")
+                    #write("::: admonition\n\n``` python")
+                    write(textwrap.dedent(f"""
+                    <blockquote>
+                    <pre><!-- <code class="python"> -->
+                        <span style="color:#900">{k}</span>{s.replace('=None','')}
+                    <!-- </code> --> </pre>
+                    """))
                     try:
-                        print(v.about)
+                        write(v.about)
                     except:
                         pass
+
+                    # write(">| | | |\n|--|--|--|")
+                    # write("<table>")
+                    write(textwrap.dedent("""
+                    <table>
+                    <colgroup>
+                      <col style="width: 15%" ><col style="width: 15%" ><col style="width: 70%" >
+                    </colgroup>
+                    <tbody>
+                    """))
+                    # write("<tbody>")
                     try:
                         for a in v._args:
                             typ = a.__class__.__name__
-                            print(f"\t| {a.name} (`{typ}`) |  {a.about} |")
-                        print("\n")
+                            #about = a.about.replace('\n',' ').strip()
+                            name = a.name
+                            default = " = "+a.default if a.default else ""
+                            about = re.sub('[\s+]', ' ', a.about.replace('\n',' '))
+                            # write(f">| {a.name} | `{typ}` |  {about} |")
+                            write(f"<tr><td>{name+default}</td><td>`{typ}`</td><td>{about}</td>")
                     except:
                         pass
+                    write(textwrap.dedent("""
+                    </tbody>
+                    </table>
+                    </blockquote>
+                    """))
+                    #write(":::\n")
             else:
                 pass
                 #print(type(v))
