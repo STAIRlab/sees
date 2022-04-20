ddc6c164a9dd4cbf873c8d9de6e7c986ec902656
claudio perez <50180406+claudioperez@users.noreply.github.com>
2022-04-20 09:11:49 -0700

    cmp

--- src/opensees/emit/__init__.py
+++ src/opensees/emit/__init__.py
@@ -3,21 +3,37 @@ from .writer import ModelWriter
 from .fedeas import FEDEAS_Writer
 
 class JSON(ModelWriter):
-    def dump(self):
-        import json
+    def dump_from_tcl(self, definitions=None):
+        import tempfile, os, json
+        with tempfile.TemporaryDirectory() as tmp:
+            path = os.path.join(tmp, 'temp.json')
+            import opensees.tcl
+            opensees.tcl.eval(f"""
+            {OpenSeesWriter(self.model).dump(definitions=definitions)}
 
-        out = {
-            "StructuralAnalysisModel":{
-                "properties": {"crdTransformations": [
-                    
-                ]},
-                "geometry": {
-                    "nodes": [
-                        {**n, "crd": [float(i) for i in n.crd]} for n in self.model.nodes
-                    ],
-                    "elements": [
-                    ]
-                }
-            }
-        }
-        return json.dumps(out, indent=2)
+            print -JSON -file {path}
+            """)
+            with open(path,"r") as f:
+                json_string = f.read()
+        return json_string
+
+
+
+
+    def dump(self, definitions=None):
+        return self.dump_from_tcl(definitions=definitions)
+        # out = {
+        #     "StructuralAnalysisModel":{
+        #         "properties": {"crdTransformations": [
+        #             
+        #         ]},
+        #         "geometry": {
+        #             "nodes": [
+        #                 {**n, "crd": [float(i) for i in n.crd]} for n in self.model.nodes
+        #             ],
+        #             "elements": [
+        #             ]
+        #         }
+        #     }
+        # }
+        # return json.dumps(out, indent=2)
