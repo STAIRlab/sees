aca6e3daeeeb39a0ea8a762d09bdbf7d47645cd4
claudio perez <50180406+claudioperez@users.noreply.github.com>
2022-05-20 13:29:17 -0700

    cmp - add ext modules to setup

--- setup.py
+++ setup.py
@@ -3,4 +3,15 @@
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(
+        ext_modules=[
+            setuptools.Extension(
+                name='opensees.libOpenSeesRT',
+                sources=[]
+            ),
+            setuptools.Extension(
+                name='opensees.libOpenSeesPyRT',
+                sources=[]
+            )
+        ]
+    )
