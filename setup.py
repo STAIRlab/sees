bef84de510ed109294d7a6318cc06530ff9d8dbb
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2022-08-13 23:22:07 -0700

    cmp

--- setup.py
+++ setup.py
@@ -3,37 +3,50 @@ import sys
 from glob import glob
 from os.path import basename, splitext
 
+import amoeba
 import setuptools
-#from setuptools import find_packages#, setup
 
 if __name__ == "__main__":
-    args = dict(
-        #package_dir = {"": "src"},
-        #packages    = ["opensees", "opensees.emit", "opensees.units"],
-        #packages = find_packages("src"),
-        # cmake_install_dir="src/opensees",
-        # cmake_args  = ["-DDependencies=Conda"],
-        name="opensees",
-        cmake_args = ["-G", "Unix Makefiles"]
+    setuptools.setup(
+        cmdclass = {"build_ext": amoeba.BuildExtension},
+        ext_modules = [
+            amoeba.CMakeExtension(
+                name = "rt",
+                install_prefix="opensees",
+                cmake_configure_options = [
+                    "-G", "Unix Makefiles", "-DDependencies=Conda"
+                ],
+                #cmake_configure_defaults = {}
+            )
+        ]
     )
 
-    if "develop" == sys.argv[1]:
-        import amoeba
-        from amoeba import setup
-        args["package_dir"] = {"opensees": "src/opensees"}
-        args["cmake_args"] += [
-            "-DCMAKE_INSTALL_PREFIX:PATH=/home/claudio/packages/opensees-pypi-test/src/opensees"
-        ]
-        try:
-            setup(**args)
+    # args = dict(
+    #     #package_dir = {"": "src"},
+    #     #packages    = ["opensees", "opensees.emit", "opensees.units"],
+    #     #packages = find_packages("src"),
+    #     # cmake_install_dir="src/opensees",
+    #     name="opensees",
+    #     cmake_args = [
+    #         "-G", "Unix Makefiles",
+    #         "-DDependencies=Conda"
+    #     ],
+    # )
+    # args["package_dir"] = {"": "src"}
 
-        except amoeba.exceptions.SKBuildInstallError:
-            if "cmake_args" in args: del args["cmake_args"]
-            del args["package_dir"]
-            setuptools.setup(**args)
+    # if "develop" == sys.argv[1]:
+    #     import amoeba
+    #     from amoeba import setup
+    #     args["cmake_args"] += [
+    #         "-DCMAKE_INSTALL_PREFIX:PATH=/home/claudio/packages/opensees-pypi-test/src/opensees"
+    #     ]
+    #     try:
+    #         setup(**args)
 
-    else:
-        from skbuild import setup
-        setup(**args)
+    #     except amoeba.exceptions.SKBuildInstallError:
+    #         if "cmake_args" in args: del args["cmake_args"]
+    #         del args["package_dir"]
+    #         setuptools.setup(**args)
+
 
 
