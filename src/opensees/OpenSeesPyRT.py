c9615cab6994a8b0158a836d9b80eddc4b8f81ba
claudio perez <50180406+claudioperez@users.noreply.github.com>
2022-09-12 11:53:40 -0700

    cmp

--- src/opensees/OpenSeesPyRT.py
+++ src/opensees/OpenSeesPyRT.py
@@ -1,6 +1,19 @@
-from .libOpenSeesRT import *
+import os
 
-from . import libOpenSeesRT
+if "OPENSEESRT_LIB" in os.environ:
+    print(os.environ)
+    import importlib.util
+    import sys
+    spec = importlib.util.spec_from_file_location("libOpenSeesRT", os.environ.get("OPENSEESRT_LIB"))
+    libOpenSeesRT = importlib.util.module_from_spec(spec)
+    sys.modules["libOpenSeesRT"] = libOpenSeesRT
+    spec.loader.exec_module(libOpenSeesRT)
+    # libOpenSeesRT.MyClass()
+
+else:
+    # from .libOpenSeesRT import *
+
+    from . import libOpenSeesRT
 
 def __getattr__(name):
     return getattr(libOpenSeesRT, name)
--- src/opensees/series.py
+++ src/opensees/series.py
@@ -3,7 +3,7 @@ import math
 def triangle(t, period=2*math.pi, amplitude=2.0):
     a, p = amplitude, period
     return 4*a/p * abs((((t-p/4)%p)+p)%p - p/2) - a
-    # return 4*amplitude/period * abs((t-period/4)%period - period/2) - amplitude
+
 
 def sawtooth(t, period=2*math.pi, amplitude = 2):
     return amplitude*(t/period - math.floor(0.5 + t/period))
